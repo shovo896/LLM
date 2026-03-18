76c174409a5b8ded2d705818632dfde028d3fa86
Shuvo <suvo10.info@gmail.com>
2026-03-19 01:23:54 +0600

    hj

--- Langchain/main.py
+++ Langchain/main.py
@@ -3,3 +3,19 @@ import os
 from constants import groqapi_key
 from langchain_groq import ChatGroq
 
+import streamlit as st
+
+os.environ['GROQ_API_KEY'] = groqapi_key
+
+
+# streamlit framework 
+
+st.title("Groq API with Langchain")
+# create a chat instance
+input_text = st.text_input("Enter your query:")
+
+## LLM integration with groq api
+
+if input_text:
+    
+
