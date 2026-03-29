0332197d43e4fc8ee558cf48af7aea957ea11a55
Shuvo <suvo10.info@gmail.com>
2026-03-29 20:14:59 +0600

    gjd

---
+++ Langchain/app1.py
@@ -0,0 +1,9 @@
+## conversational Q&A chatbot 
+
+import streamlit as st 
+
+from langchain.schema import HumanMessage,SystemMessage,AIMessage
+from langchain.chat_models import ChatOpenAI
+
+
+## streamlit app 
