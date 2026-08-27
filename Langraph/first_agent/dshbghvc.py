df92142903f86a83d477b5dc84bbbdc25d631b27
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-27 14:49:14 +0600

    cfiji

--- Langraph/first_agent/dshbghvc.py
+++ Langraph/first_agent/dshbghvc.py
@@ -10,3 +10,18 @@ load_dotenv()
 class AgentState(TypedDict): 
     messages: List[BaseMessage] 
     
+llm = ChatOpenAI(
+    model="meta-llama/llama-3.1-8b-instruct:free",  
+    api_key=os.getenv("OPENROUTER_API_KEY"),         
+    base_url="https://openrouter.ai/api/v1",          
+    default_headers={
+        "HTTP-Referer": "http://localhost",          
+        "X-Title": "AgentBot",                       
+    }
+)
+
+def process(state:AgentState) -> AgentState: 
+    response= llm.invoke(state["messages"]) 
+    state["messages"].append(response)
+    return state
+    
