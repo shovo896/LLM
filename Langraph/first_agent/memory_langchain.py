4d788eacacc6f4af54ebbd72e2ee81290af1d717
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-27 18:50:11 +0600

    jnkj

--- Langraph/first_agent/memory_langchain.py
+++ Langraph/first_agent/memory_langchain.py
@@ -1,21 +1,15 @@
-import os 
-from typing import List, TypedDict,Union 
-from langchain_core.messages import HumanMessage, AIMessage, BaseMessage 
-from langchain_openai import ChatOpenAI 
-from langgraph.graph import END, START, StateGraph 
-from dotenv import load_dotenv 
+import os
+from typing import List, TypedDict
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langchain_openai import ChatOpenAI
+from langgraph.graph import END, START, StateGraph
+from dotenv import load_dotenv
+
 load_dotenv()
 
-
-
-
-
 class AgentState(TypedDict):
-    #messages: List[HumanMessage]
-    #messages_ai:List[Union[AIMessage]] 
-    messages: Union[HumanMessage, AIMessage]
-    
-    
+    messages: List[BaseMessage]
+
 llm = ChatOpenAI(
     model="nvidia/nemotron-3.5-lightning:free",
     api_key=os.getenv("OPENROUTER_API_KEY"),
@@ -27,36 +21,19 @@ llm = ChatOpenAI(
 )
 
 def process(state: AgentState) -> AgentState:
-    """this node will solve request you input and return the response from the LLM"""
-    
     response = llm.invoke(state["messages"])
     state["messages"].append(AIMessage(content=response.content))
-    print(f"\nAgent: {response.content}") 
-    
-    return state 
+    print(f"\nAgent: {response.content}")
+    return state
 
-with open("logging.txt", "a") as log_file:
-    log_file.write("Your conversation logged here:\n") 
-    for message in conversation_history:
-        if isinstance(message, HumanMessage):
-            log_file.write(f"You: {message.content}\n")
-        elif isinstance(message, AIMessage):
-            log_file.write(f"Agent: {message.content}\n")
-    file.write("\n")  # Add a newline after each conversation for separation    
-    
-print("Conversation logged in logging.txt")
-
-
-graph = StateGraph(AgentState) 
+graph = StateGraph(AgentState)
 graph.add_node("agent", process)
-graph.add_edge(START, 'agent')
-graph.add_edge('agent', END)
-agent=graph.compile() 
+graph.add_edge(START, "agent")
+graph.add_edge("agent", END)
+agent = graph.compile()
 
-
-conversation_history=[]
-user_input = input("You: ") 
-
+conversation_history = []
+user_input = input("You: ")
 
 while user_input != "exit":
     conversation_history.append(HumanMessage(content=user_input))
@@ -65,11 +42,13 @@ while user_input != "exit":
     conversation_history = result["messages"]
     user_input = input("You: ")
 
+with open("logging.txt", "a") as log_file:
+    log_file.write("Your conversation logged here:\n")
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            log_file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            log_file.write(f"Agent: {message.content}\n")
+    log_file.write("\n")
 
-
-
-
-    
-    
-
-
+print("Conversation logged in logging.txt")
