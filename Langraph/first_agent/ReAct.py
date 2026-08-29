79397daab7104775d37f43b8c038a6d821430c58
Shuvo <177408979+shovo896@users.noreply.github.com>
2026-08-29 11:06:50 +0600

    gyjkobm

--- Langraph/first_agent/ReAct.py
+++ Langraph/first_agent/ReAct.py
@@ -11,6 +11,26 @@ from langgraph.prebuilt import ToolNode
 load_dotenv()
 
 
-email=Annotated[str, "The email address of the user"] 
+''''email=Annotated[str, "The email address of the user"] 
 print("Email:", email)
-print(email.__metadata__) 
+###print(email.__metadata__) 
+
+
+#without a reducer 
+
+state={"messages": ["Hi!"]}
+update= {"messages":["Nice to meet you!"]}
+new_state={"messages": ["Nice to meet you!"]}
+
+
+
+
+#with a reducer 
+state={"messages": ["Hi!"]}
+update= {"messages":["Nice to meet you!"]}
+new_state={"messages": ["Hi!", "Nice to meet you!"]} '''
+
+class AgentState(TypedDict):
+    messages: Annotated[List[BaseMessage], "The list of messages exchanged between the agent and the user"]
+    email: Annotated[str, "The email address of the user"]
+
